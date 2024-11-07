997c828693d6dd00776c124046b236a5cf432470
artes26 <ayush16r@gmail.com>
2024-11-08 00:55:45 +0530

    init commit

---
+++ src/Q1.java
@@ -0,0 +1,20 @@
+/*
+Question-1:
+Write a java program to display following messages.
+Hello “ITERIAN”
+Welcome to Siksha ‘O’ Anusandhan Family.
+Welcome to “Introduction to Computer Laboratory”
+Java is fun for All !!
+
+ */
+public class Q1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello \"IITERIAN\"");
+        System.out.println("welcome to Siksha 'o' Anusandhan Family. ");
+        System.out.println("Welcome to \" introduction to computer laboratory\"");
+        System.out.println("java is fun for all ");
+
+    }
+}
---
+++ src/Q2.java
@@ -0,0 +1,14 @@
+/*
+ simmilar pattern code through print statment
+ */
+
+public class Q2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("******      ********");
+        System.out.println("     *      *      *");
+        System.out.println("     ********      *******");
+
+    }
+}
